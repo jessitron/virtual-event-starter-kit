475bbbed02ac72efbd9c5077f3b8f70c9c2821f3
Jessica Kerr <jessitron@gmail.com>
2023-03-26 15:56:36 +0100

    Try to send traces to hnhy

--- pages/tracing.ts
+++ pages/tracing.ts
@@ -1,6 +1,10 @@
 // pilfered from https://github.com/open-telemetry/opentelemetry-demo/blob/9abfe562626227cef9d91e41fe9ce5f01d1dd0ff/src/frontend/utils/telemetry/FrontendTracer.ts
 
-import { CompositePropagator, W3CBaggagePropagator, W3CTraceContextPropagator } from '@opentelemetry/core';
+import {
+  CompositePropagator,
+  W3CBaggagePropagator,
+  W3CTraceContextPropagator
+} from '@opentelemetry/core';
 import { WebTracerProvider } from '@opentelemetry/sdk-trace-web';
 import { SimpleSpanProcessor } from '@opentelemetry/sdk-trace-base';
 import { registerInstrumentations } from '@opentelemetry/instrumentation';
@@ -9,27 +13,30 @@ import { Resource, detectResources, browserDetector } from '@opentelemetry/resou
 import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
 
-console.log("JESS tracing was loaded")
-
+console.log('JESS tracing was loaded');
 
 const FrontendTracer = async (collectorString: string) => {
   const { ZoneContextManager } = await import('@opentelemetry/context-zone');
-  console.log("past the await import")
+  console.log('past the await import');
 
   let resource = new Resource({
-    [SemanticResourceAttributes.SERVICE_NAME]: "jess-the-client-side",
+    [SemanticResourceAttributes.SERVICE_NAME]: 'jess-the-client-side'
   });
 
-  const detectedResources = await detectResources({detectors:[browserDetector]});
+  const detectedResources = await detectResources({ detectors: [browserDetector] });
   resource = resource.merge(detectedResources);
   const provider = new WebTracerProvider({
     resource
   });
 
+const { NEXT_PUBLIC_HONEYCOMB_API_KEY = '' } =
+typeof window !== 'undefined' ? (window as any).ENV : {};
+
   provider.addSpanProcessor(
     new SimpleSpanProcessor(
       new OTLPTraceExporter({
         url: 'https://api.honeycomb.io/v1/traces',
+        headers: { 'x-honeycomb-team': NEXT_PUBLIC_HONEYCOMB_API_KEY }
       })
     )
   );
@@ -39,8 +46,8 @@ const FrontendTracer = async (collectorString: string) => {
   provider.register({
     contextManager,
     propagator: new CompositePropagator({
-      propagators: [new W3CBaggagePropagator(), new W3CTraceContextPropagator()],
-    }),
+      propagators: [new W3CBaggagePropagator(), new W3CTraceContextPropagator()]
+    })
   });
 
   registerInstrumentations({
@@ -52,10 +59,10 @@ const FrontendTracer = async (collectorString: string) => {
           clearTimingResources: true,
           applyCustomAttributesOnSpan(span) {
             span.setAttribute('app.synthetic_request', 'false');
-          },
-        },
-      }),
-    ],
+          }
+        }
+      })
+    ]
   });
 };
 
